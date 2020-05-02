5ca0b3b55aa5b1cd7b13e390eac3a0e76f0df37e
Yasas4D <yasassandeepa007@gmail.com>
2020-05-02 19:34:23 +0530

    'restructured'

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package arrays;
 
 import java.util.Scanner;
@@ -105,7 +101,7 @@ int[]a = {5,2,1,10,12,2,4,9};
             
         }
         
-        System.out.println(counter);
+       // System.out.println(counter);
         System.out.println("Max is :"+max);
         
         System.out.println("Min is :"+min);
