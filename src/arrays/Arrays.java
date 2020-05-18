c79391cb323d9f46e9e310c7cb47982eeef5f8e9
Yasas4D <yasassandeepa007@gmail.com>
2020-05-18 19:22:36 +0530

    Remove the Counter

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -108,32 +108,7 @@ int[]a = {5,2,1,10,12,2,4,9};
     }
 }
 
-//int count  = 0;
-//
-//        String[] p={"dddd","aaaa","cccc","pppp"};
-//        for (int i = 0; i < p.length; i++) {
-//            count=count+p[i].length();
-//            System.out.println(p[i].toUpperCase());
-//           
-//            
-//        }
-//        System.out.println(count);
-//        char[] m=new char[count];
-//        int count2=0;
-//        for (int i = 0; i < p.length; i++) {
-//            
-//            for (int j = 0; j < p[i].length(); j++) {
-//                m[count2]=p[i].charAt(j);
-//                count2++;
-//            }
-//            //
-//            
-//            
-//        }
-//        
-//        for (int i = 0; i < m.length; i++) {
-//            System.out.print(m[i]+" ");
-//        }
+
 
         
 //counting vowels
